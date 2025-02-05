0fc6d7e895ff209e5c10c365d566adeea3b650ba
amoghaaa <amoghavemuri1234@gmail.com>
2025-02-05 20:23:00 +0530

    Add inheritance example with Animal and Dog classes

---
+++ Animal.java
@@ -0,0 +1,14 @@
+// Base class
+public class Animal {
+    String name;
+
+    // Constructor
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    // Method to make a sound
+    public void makeSound() {
+        System.out.println(name + " makes a sound.");
+    }
+}
